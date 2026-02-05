7e2a759ee0677388ba6a9300af4fd7a01a2e7314
bananasplit26 <andersonlam12456234@gmail.com>
2026-02-04 16:41:40 -0800

    Updated with one project

--- src/config.ts
+++ src/config.ts
@@ -10,14 +10,14 @@ export const siteConfig = {
   },
   aboutMe:
     "",
-  skills: ["Python", "C/C++", "CSS", "HTML", "Java", "React", "NumPy", "Matplotlib", "Tkinter", "Figma", "Git"],
+  skills: ["Python", "C/C++", "CSS", "HTML", "Java", "React", "Figma"],
   projects: [
     {
-      name: "1",
+      name: "GymPal",
       description:
-        "1",
-      link: "1",
-      skills: ["1", "1", "1"],
+        "Track your exercises, hydration, calorie intake, and browse through recommended workouts and meals based on user-created fitness goals.",
+      link: "https://github.com/anderl4/GymPal",
+      skills: ["React", "Node.js", "Firebase", "Figma"],
     },
     {
       name: "2",
@@ -34,47 +34,13 @@ export const siteConfig = {
       skills: ["3", "3", "3"],
     },
   ],
-  // experience: [
-  //   {
-  //     company: "Tech Company",
-  //     title: "Senior Software Engineer",
-  //     dateRange: "Jan 2022 - Present",
-  //     bullets: [
-  //       "Led development of microservices architecture serving 1M+ users",
-  //       "Reduced API response times by 40% through optimization",
-  //       "Mentored team of 5 junior developers",
-  //     ],
-  //   },
-  //   {
-  //     company: "Startup Inc",
-  //     title: "Full Stack Developer",
-  //     dateRange: "Jun 2020 - Dec 2021",
-  //     bullets: [
-  //       "Built and launched MVP product from scratch using React and Node.js",
-  //       "Implemented CI/CD pipeline reducing deployment time by 60%",
-  //       "Collaborated with product team to define technical requirements",
-  //     ],
-  //   },
-  //   {
-  //     company: "Digital Agency",
-  //     title: "Frontend Developer",
-  //     dateRange: "Aug 2018 - May 2020",
-  //     bullets: [
-  //       "Developed responsive web applications for 20+ clients",
-  //       "Improved site performance scores by 35% on average",
-  //       "Introduced modern JavaScript frameworks to legacy codebases",
-  //     ],
-  //   },
-  // ],
   education: [
     {
       school: "University of California, Irvine",
       degree: "Bachelor of Science in Computer Science",
-      dateRange: "2020 - 2024",
+      dateRange: "October 2020 - June 2024",
       achievements: [
-        "1",
-        "2",
-        "3",
+        "Specialized in Information"
       ],
     }
   ],
